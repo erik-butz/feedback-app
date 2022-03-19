6011f02e1c1e719a1a4c1d65090976b63e975943
Erik Butz <erikbutzlive@gmail.com>
2022-03-19 15:21:56 -0500

    added a spinner for when loading the page. Used boolean state to be set to false after we fetc the backend data and then ti shows data on page

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,17 +1,20 @@
 import { motion, AnimatePresence } from 'framer-motion'
 import FeedbackItem from './FeedbackItem'
+import Spinner from './shared/Spinner'
 import { useContext } from 'react'
 import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackList() {
+  const { feedback, isLoading } = useContext(FeedbackContext)
 
-  const { feedback } = useContext(FeedbackContext)
-
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No Feedback Yet</p>
   }
-
-  return (
+  
+  //conditional statement of checking if isLoading is true. It's set to false after we fetch the data from our backend. 
+  return isLoading ? (
+    <Spinner/>
+  ) : (
     <div className='feedback-list'>
       <AnimatePresence>
         {feedback.map((item) => (
@@ -21,10 +24,7 @@ function FeedbackList() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <FeedbackItem
-              key={item.id}
-              item={item}
-            />
+            <FeedbackItem key={item.id} item={item} />
           </motion.div>
         ))}
       </AnimatePresence>
